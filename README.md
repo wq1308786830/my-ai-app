de1750d1a346fb35e70a919c5a519227cc1be617
Russell <flyingcycling@gmail.com>
2025-08-20 23:10:06 +0800

    feat: ollama部署以及对接ai-sdk

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### 模型部署
 `CUDA_VISIBLE_DEVICES=0 vllm serve Qwen/Qwen3-1.7B   --port 8888   --host 0.0.0.0   --gpu-memory-utilization 0.85   --max-model-len 4096   --served-model-name qwen1.7b`
+
+#### Ollama部署
+ - windows直接安装修改非C盘安装位置`OllamaSetup.exe /DIR="D:\AI_Tools\Ollama"`
+ - `netstat -ano | findstr :11434`查看ollama端口占用
+ - `taskkill /f /pid  5420`杀掉ollama安装后自动启动的进程
+ - `Ollama serve`启动Ollama
+ - `ollama pull qwen3:8b`下载模型
+ - `ollama run qwen3:8b`同时启用交互式命令行以及http接口
+ - `ollama serve`http服务模式启动所有模型
--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,6 +4,14 @@ import { streamText } from 'ai';
 import { createStreamableValue } from '@ai-sdk/rsc';
 import {CustomChatLanguageModel} from "@/app/custom-chat-language-model";
 import {LanguageModelV2} from "@ai-sdk/provider";
+import {createOllama} from 'ollama-ai-provider-v2';
+
+const ollamaPrefixUrl = 'http://localhost:11434/api';
+const ollamaChatApi = `${ollamaPrefixUrl}/chat`
+const ollama = createOllama({
+    // optional settings, e.g.
+    baseURL: ollamaPrefixUrl,
+});
 
 export interface Message {
     role: 'user' | 'assistant';
@@ -17,7 +25,8 @@ export async function continueConversation(history: Message[]) {
 
     (async () => {
         const { textStream } = streamText({
-            model: new CustomChatLanguageModel('qwen1.7b') as any,
+            // model: new CustomChatLanguageModel('qwen1.7b') as any,
+            model: ollama('qwen3:8b'),
             system: "你是一个中医大牛",
             messages: history,
         });
